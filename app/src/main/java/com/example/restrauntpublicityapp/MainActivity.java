2fa15aa9dcb7b2162674e137096b8ef65c7e99e9
Shubh Gupta <shubh.gupta2021@vitstudent.ac.in>
2022-07-22 17:52:15 +0530

    commit: 1.5

--- app/src/main/java/com/example/restrauntpublicityapp/MainActivity.java
+++ app/src/main/java/com/example/restrauntpublicityapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.restrauntpublicityapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void loc(View v){
+
+    }
+
+    public void site(View v){
+
+
     }
 }
